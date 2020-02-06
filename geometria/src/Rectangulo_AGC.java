9d060777c396a7c478e0f2c512c1ed4708b380d7
Adrian Granero <adriangran17@gmail.com>
2020-02-06 09:49:39 +0100

    version comentada

--- geometria/src/Rectangulo_AGC.java
+++ geometria/src/Rectangulo_AGC.java
@@ -1,8 +1,18 @@
-
+/**
+ * 
+ * @author Adrian Granero Caparros
+ * @version 1.2
+ *
+ */
 public class Rectangulo_AGC extends FiguraGeometrica_AGC {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * 
+	 * @param tipoFigura
+	 * @param lG Un lado del rectangulo	
+	 * @param lP Otro lado del rectangulo
+	 */
 	public Rectangulo_AGC(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
@@ -10,11 +20,19 @@ public class Rectangulo_AGC extends FiguraGeometrica_AGC {
 	}
 
 	@Override
+	/**
+	 * Calcula el area
+	 * @return devuelve el area
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
 	@Override
+	/**
+	 * Calcula el perimetro
+	 * @return devuelve el perimetro
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
