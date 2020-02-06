dd284bb8b3d8fbf572b2fefbbface5f50f85e00a
Adrian Granero <adriangran17@gmail.com>
2020-02-06 08:43:38 +0100

    Versión refactorizada

--- geometria/src/Circulo_AGC.java
+++ geometria/src/Circulo_AGC.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_AGC extends FiguraGeometrica_AGC {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_AGC(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_AGC.java
+++ geometria/src/FiguraGeometrica_AGC.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_AGC {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_AGC(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
